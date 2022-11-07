eadec7174b186844fabe8c74ac781978316f3272
mnekx <karimnembac@gmail.com>
2022-11-07 18:56:52 +0300

    create post ui

--- src/features/home/PostMessages.js
+++ src/features/home/PostMessages.js
@@ -1,5 +1,11 @@
 import styles from './post.module.css';
+import SendIcon from '@mui/icons-material/Send';
 
-const PostMessage = () => <form className={styles.Form}><input type='textarea' placeholder="enter message"/><button type="submit">post</button></form>
+const PostMessage = () => (
+  <form className={styles.Form}>
+    <input type='textarea' placeholder='enter message' className={styles.Input}/>
+    <button type='submit' className={styles.Button}><SendIcon/></button>
+  </form>
+);
 
-export default PostMessage
+export default PostMessage;
