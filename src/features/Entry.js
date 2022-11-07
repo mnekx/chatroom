487912e4dd22510182a9ffd97ad169f58ef1a98b
mnekx <karimnembac@gmail.com>
2022-11-08 02:27:15 +0300

    update Entry component

--- src/features/Entry.js
+++ src/features/Entry.js
@@ -1,5 +1,5 @@
 import styles from './entry.module.css';
 
-const Entry = () => <form><input type="text" placeholder="Name" className={styles.Input} /><button className={styles.Button} type="submit">Enter</button></form>
+const Entry = () => <form className={styles.Form}><input type="text" placeholder="Name" className={styles.Input} /><button className={styles.Button} type="submit">Enter</button></form>
 
 export default Entry;
