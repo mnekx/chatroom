8e84b3036bbb310741134517cc66b9e946c46759
mnekx <karimnembac@gmail.com>
2022-11-08 09:57:15 +0300

    create tests

--- src/features/home/list-messages/ListMessages.js
+++ src/features/home/list-messages/ListMessages.js
@@ -7,7 +7,7 @@ const ListMessage = () => {
   const currUser = useUserState();
   const messages = useSelector(state => state.messages);
   return (
-  <ul className={styles.Root}>{messages.map(((message, index) => <li className={currUser.name===message.user?styles.Mine: ''} key={index}><Message message={message}/></li>))}</ul>
+  <ul data-testid={'list-messages'} className={styles.Root}>{messages.map(((message, index) => <li className={currUser.name===message.user?styles.Mine: ''} key={index}><Message message={message}/></li>))}</ul>
 )};
 
 export default ListMessage;
