21466814d66d03d58e80d7ccf59e8e440479e3bb
mnekx <karimnembac@gmail.com>
2022-11-08 10:12:07 +0300

    style the header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import { UserProvider } from './context/user';
+import Header from './features/Header'
 
 function App() {
   return (
     <UserProvider>
       <div className='App'>
         <header className='App-header'>
-          <nav>this is nav</nav>
+          <Header/>
         </header>
         <main>
           <Outlet />
